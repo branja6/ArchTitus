84b79c44b87efd43dd4891caaee348e74d3bd644
branja6 <71647226+branja6@users.noreply.github.com>
2021-11-29 12:32:44 +0100

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -29,8 +29,6 @@ PKGS=(
 'dxvk-bin' # DXVK DirectX to Vulcan
 'github-desktop-bin' # Github Desktop sync
 'lightly-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
 'nerd-fonts-fira-code'
 'nordic-darker-standard-buttons-theme'
 'nordic-darker-theme'
